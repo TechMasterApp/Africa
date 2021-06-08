ae242225841cc016ce2ee130916c26d84dc27f75
Gaurav Bhasin <bhasingaurav@gmail.com>
2021-06-08 19:07:41 -0400

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Gaurav Bhasin on 6/8/21.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
